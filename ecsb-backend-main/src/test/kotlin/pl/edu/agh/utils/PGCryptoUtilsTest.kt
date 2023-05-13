7c7c42ce964f9cee6ec94355e824c705654939df
Lukasz Pawlak <lpawlak@student.agh.edu.pl>
2023-05-13 16:41:56 +0200

    [CU-862jttawa] Added pg crypto tests

---
+++ ecsb-backend-main/src/test/kotlin/pl/edu/agh/utils/PGCryptoUtilsTest.kt
@@ -0,0 +1,30 @@
+package pl.edu.agh.utils
+
+import org.jetbrains.exposed.sql.QueryBuilder
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import pl.edu.agh.auth.domain.Password
+import pl.edu.agh.utils.PGCryptoUtils.toDbValue
+import kotlin.test.assertEquals
+
+class PGCryptoUtilsTest {
+
+    @Test
+    fun selectEncryptedPassword() {
+        val cryptExpr = PGCryptoUtils.CryptExpression("kolumna", Password("tajne"))
+
+        val queryBuilder = QueryBuilder(false)
+        cryptExpr.toQueryBuilder(queryBuilder)
+
+        assertEquals("kolumna = crypt('tajne', kolumna)", queryBuilder.toString())
+    }
+
+    @Test
+    fun toDbValue() {
+        val queryBuilder = QueryBuilder(false)
+        Password("tajne").toDbValue().toQueryBuilder(queryBuilder)
+
+        assertEquals("crypt('tajne', gen_salt('bf'))", queryBuilder.toString())
+    }
+}
