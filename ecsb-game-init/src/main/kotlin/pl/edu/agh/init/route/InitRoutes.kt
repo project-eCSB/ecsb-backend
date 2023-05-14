63cc22f407a92636848f15ec74bf2655d286c72c
Lukasz Pawlak <lpawlak@student.agh.edu.pl>
2023-05-14 15:32:07 +0200

    [CU-862ju4vya] Moved init routes to /

--- ecsb-game-init/src/main/kotlin/pl/edu/agh/init/route/InitRoutes.kt
+++ ecsb-game-init/src/main/kotlin/pl/edu/agh/init/route/InitRoutes.kt
@@ -30,74 +30,72 @@ object InitRoutes {
         val gameConfigService by inject<GameConfigService>()
 
         routing {
-            route("/init") {
-                authenticate(Token.GAME_TOKEN, Role.USER) {
-                    get("/gameStatus/") {
-                        Utils.handleOutput(call) {
-                            either {
-                                val (gameSessionId, loginUserId) = getGameUser(call).toEither { HttpStatusCode.Unauthorized to "Jwt malformed" }
-                                    .bind()
+            authenticate(Token.GAME_TOKEN, Role.USER) {
+                get("/gameStatus/") {
+                    Utils.handleOutput(call) {
+                        either {
+                            val (gameSessionId, loginUserId) = getGameUser(call).toEither { HttpStatusCode.Unauthorized to "Jwt malformed" }
+                                .bind()
 
-                                gameConfigService.getGameUserStatus(gameSessionId, loginUserId)
-                                    .toEither { HttpStatusCode.NotFound to "Resource not found" }.bind()
-                            }.responsePair(PlayerStatus.serializer())
-                        }
-                    }
-                    get("/settings") {
-                        Utils.handleOutput(call) {
-                            either {
-                                val (gameSessionId, _) = getGameUser(call).toEither { HttpStatusCode.Unauthorized to "Couldn't find payload" }
-                                    .bind()
-
-                                logger.info("get game for gameSessionId $gameSessionId")
-                                gameConfigService.getGameInfo(gameSessionId)
-                                    .toEither { HttpStatusCode.NotFound to "Resource not found" }.bind()
-                            }.responsePair(GameSessionView.serializer())
-                        }
+                            gameConfigService.getGameUserStatus(gameSessionId, loginUserId)
+                                .toEither { HttpStatusCode.NotFound to "Resource not found" }.bind()
+                        }.responsePair(PlayerStatus.serializer())
                     }
                 }
-                authenticate(Token.LOGIN_USER_TOKEN, Role.ADMIN) {
-                    post("/admin/createGame") {
-                        Utils.handleOutput(call) {
-                            either {
-                                val gameInitParameters = Utils.getBody<GameInitParameters>(call).bind()
-                                val (_, _, loginUserId) = getLoggedUser(call)
+                get("/settings") {
+                    Utils.handleOutput(call) {
+                        either {
+                            val (gameSessionId, _) = getGameUser(call).toEither { HttpStatusCode.Unauthorized to "Couldn't find payload" }
+                                .bind()
 
-                                // TODO ADD TILED PARSER HERE <-
-                                val coords = Coordinates(3, 3)
-                                val direction = Direction.DOWN
-
-                                gameConfigService.createGame(gameInitParameters, coords, direction, loginUserId)
-                                    .toEither().mapLeft { it.toResponse() }.bind()
-                            }.responsePair(GameSessionId.serializer())
-                        }
-                    }
-                    get("/admin/settings/{gameSessionId}") {
-                        Utils.handleOutput(call) {
-                            either {
-                                val gameSessionId: GameSessionId =
-                                    getParam("gameSessionId") { GameSessionId(it) }.bind()
-
-                                logger.info("get game for gameSessionId $gameSessionId")
-                                gameConfigService.getGameInfo(gameSessionId)
-                                    .toEither { HttpStatusCode.NotFound to "Resource not found" }.bind()
-                            }.responsePair(GameSessionView.serializer())
-                        }
+                            logger.info("get game for gameSessionId $gameSessionId")
+                            gameConfigService.getGameInfo(gameSessionId)
+                                .toEither { HttpStatusCode.NotFound to "Resource not found" }.bind()
+                        }.responsePair(GameSessionView.serializer())
                     }
                 }
-                authenticate(Token.LOGIN_USER_TOKEN, Role.USER, Role.ADMIN) {
-                    post("/getGameToken") {
-                        Utils.handleOutput(call) {
-                            either {
-                                val (_, roles, loginUserId) = getLoggedUser(call)
-                                val gameJoinRequest = Utils.getBody<GameJoinCodeRequest>(call).bind()
-                                val rolesNel = roles.toNonEmptyListOrNone()
-                                    .toEither { HttpStatusCode.Unauthorized to "No roles found for user" }.bind()
+            }
+            authenticate(Token.LOGIN_USER_TOKEN, Role.ADMIN) {
+                post("/admin/createGame") {
+                    Utils.handleOutput(call) {
+                        either {
+                            val gameInitParameters = Utils.getBody<GameInitParameters>(call).bind()
+                            val (_, _, loginUserId) = getLoggedUser(call)
 
-                                gameConfigService.joinToGame(gameJoinRequest, loginUserId, rolesNel).toEither()
-                                    .mapLeft { it.toResponse() }.bind()
-                            }.responsePair(GameJoinResponse.serializer())
-                        }
+                            // TODO ADD TILED PARSER HERE <-
+                            val coords = Coordinates(3, 3)
+                            val direction = Direction.DOWN
+
+                            gameConfigService.createGame(gameInitParameters, coords, direction, loginUserId)
+                                .toEither().mapLeft { it.toResponse() }.bind()
+                        }.responsePair(GameSessionId.serializer())
+                    }
+                }
+                get("/admin/settings/{gameSessionId}") {
+                    Utils.handleOutput(call) {
+                        either {
+                            val gameSessionId: GameSessionId =
+                                getParam("gameSessionId") { GameSessionId(it) }.bind()
+
+                            logger.info("get game for gameSessionId $gameSessionId")
+                            gameConfigService.getGameInfo(gameSessionId)
+                                .toEither { HttpStatusCode.NotFound to "Resource not found" }.bind()
+                        }.responsePair(GameSessionView.serializer())
+                    }
+                }
+            }
+            authenticate(Token.LOGIN_USER_TOKEN, Role.USER, Role.ADMIN) {
+                post("/getGameToken") {
+                    Utils.handleOutput(call) {
+                        either {
+                            val (_, roles, loginUserId) = getLoggedUser(call)
+                            val gameJoinRequest = Utils.getBody<GameJoinCodeRequest>(call).bind()
+                            val rolesNel = roles.toNonEmptyListOrNone()
+                                .toEither { HttpStatusCode.Unauthorized to "No roles found for user" }.bind()
+
+                            gameConfigService.joinToGame(gameJoinRequest, loginUserId, rolesNel).toEither()
+                                .mapLeft { it.toResponse() }.bind()
+                        }.responsePair(GameJoinResponse.serializer())
                     }
                 }
             }
