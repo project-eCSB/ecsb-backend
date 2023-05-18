1a966809c1829410750a390d9fe5e1feead9319e
Michał Wójcik <michwoj01@gmail.com>
2023-05-18 10:08:40 +0200

    [CU-862jp6k9g]
    - fixed order in script

--- dbscripts/init006.sql
+++ dbscripts/init006.sql
@@ -2,7 +2,18 @@ alter table game_session_user_classes
     rename column NAME to CLASS_NAME;
 
 alter table game_session_user_classes
-    drop constraint GAME_SESSION_USER_CLASSES_RESOURCE_ID_FK,
+    drop constraint GAME_SESSION_USER_CLASSES_RESOURCE_ID_FK;
+
+alter table PLAYER_RESOURCE
+    drop constraint PLAYER_RESOURCE_RESOURCE_ID_FK;
+
+truncate table GAME_SESSION_RESOURCE cascade;
+
+drop table GAME_SESSION_RESOURCE;
+
+truncate table game_session_user_classes cascade;
+
+alter table game_session_user_classes
     drop column PRODUCED_RESOURCE_ID,
     add column RESOURCE_NAME         varchar not null default 'bread',
     add column RESOURCE_SPRITE_INDEX int     not null default 1,
@@ -12,13 +23,8 @@ alter table game_session_user_classes
 create unique index GAME_SESSION_RESOURCES_UNIQUE_INDEX on GAME_SESSION_USER_CLASSES (game_session_id, RESOURCE_NAME);
 
 alter table PLAYER_RESOURCE
-    drop constraint PLAYER_RESOURCE_RESOURCE_ID_FK,
     drop column resource_id,
     add column RESOURCE_NAME varchar not null default 'bread',
     add constraint PLAYER_RESOURCE_PK PRIMARY KEY (GAME_SESSION_ID, PLAYER_ID, RESOURCE_NAME),
     add constraint PLAYER_RESOURCE_FK foreign key (GAME_SESSION_ID, RESOURCE_NAME) references game_session_user_classes (game_session_id, resource_name);
-
-truncate table GAME_SESSION_RESOURCE cascade;
-
-drop table GAME_SESSION_RESOURCE;
 
