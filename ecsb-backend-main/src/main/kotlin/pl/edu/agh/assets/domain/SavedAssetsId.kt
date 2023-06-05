72430e3ed160f997005cdf3af9eca66a478c260c
Lukasz Pawlak <lpawlak@student.agh.edu.pl>
2023-06-05 18:10:10 +0200

    [game-init-improvements] Changes after review

--- ecsb-backend-main/src/main/kotlin/pl/edu/agh/assets/domain/SavedAssetsId.kt
+++ ecsb-backend-main/src/main/kotlin/pl/edu/agh/assets/domain/SavedAssetsId.kt
@@ -1,12 +1,7 @@
 package pl.edu.agh.assets.domain
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.nullable
 
 @JvmInline
 @Serializable
 value class SavedAssetsId(val value: Int)
-
-fun main() {
-    val a = SavedAssetsId.serializer().nullable
-}
