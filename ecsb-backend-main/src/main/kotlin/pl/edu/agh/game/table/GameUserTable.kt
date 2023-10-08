d957209dd8bf142ef5d67ed20e752f5a6520384a
Łukasz Pawlak <73822063+lpawlak1@users.noreply.github.com>
2023-10-08 22:15:08 +0200

    [join-to-game-fix] Fix to enable to join to game as this dont work now (#125)

--- ecsb-backend-main/src/main/kotlin/pl/edu/agh/game/table/GameUserTable.kt
+++ ecsb-backend-main/src/main/kotlin/pl/edu/agh/game/table/GameUserTable.kt
@@ -7,8 +7,10 @@ import pl.edu.agh.auth.domain.LoginUserId
 import pl.edu.agh.auth.domain.loginUserId
 import pl.edu.agh.domain.*
 import pl.edu.agh.game.domain.GameUserDto
-import pl.edu.agh.utils.*
-import pl.edu.agh.utils.NonNegInt.Companion.nonNegDbWrapper
+import pl.edu.agh.utils.intWrapper
+import pl.edu.agh.utils.longWrapper
+import pl.edu.agh.utils.nullableStringWrapper
+import pl.edu.agh.utils.stringWrapper
 
 object GameUserTable : Table("GAME_USER") {
     val loginUserId: Column<LoginUserId> = loginUserId("LOGIN_USER_ID")
@@ -16,7 +18,6 @@ object GameUserTable : Table("GAME_USER") {
     val className: Column<GameClassName> = stringWrapper(GameClassName::value, ::GameClassName)("CLASS_NAME")
     val gameSessionId: Column<GameSessionId> = intWrapper(GameSessionId::value, ::GameSessionId)("GAME_SESSION_ID")
     val money: Column<Money> = longWrapper(Money::value, ::Money)("MONEY")
-    val time: Column<NonNegInt> = nonNegDbWrapper("TIME")
     val inGame: Column<Boolean> = bool("IN_GAME").default(true)
     val busyStatus: Column<InteractionStatus> =
         nullableStringWrapper(InteractionStatus::toDB, InteractionStatus::fromDB)("BUSY_STATUS")
