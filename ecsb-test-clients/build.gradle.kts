57e49bafc36bf443d612fcc29e12a0103f713e40
Michał Wójcik <michwoj01@gmail.com>
2024-04-28 22:17:31 +0200

    update KTOR to 2.3.10

--- ecsb-test-clients/build.gradle.kts
+++ ecsb-test-clients/build.gradle.kts
@@ -6,12 +6,12 @@ application {
 
 dependencies {
     implementation(project(mapOf("path" to ":ecsb-backend-main")))
-    implementation("io.ktor:ktor-client-java:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktorVersion")
     implementation(project(mapOf("path" to ":ecsb-game-init")))
-    implementation("io.ktor:ktor-client-encoding:2.2.3")
     implementation(project(mapOf("path" to ":ecsb-moving")))
-    implementation("io.ktor:ktor-client-logging-jvm:2.2.3")
-    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
     implementation(project(mapOf("path" to ":ecsb-chat")))
+    implementation("io.ktor:ktor-client-java-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-encoding-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-websockets-jvm:$ktorVersion")
 }
