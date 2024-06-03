a0177b9ae2c2346b8748ca8a86cf79cfbf888847
Michał Wójcik <michwoj01@gmail.com>
2024-06-03 14:01:40 +0200

    fix asset route

--- ecsb-game-init/src/main/kotlin/pl/edu/agh/logs/domain/LogsMessage.kt
+++ ecsb-game-init/src/main/kotlin/pl/edu/agh/logs/domain/LogsMessage.kt
@@ -3,15 +3,9 @@ package pl.edu.agh.logs.domain
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import pl.edu.agh.domain.PlayerId
-import pl.edu.agh.travel.domain.TravelName
 
 @Serializable
 sealed interface LogsMessage {
-
-    @Serializable
-    @SerialName("game/started")
-    data class TravelChange(val travelName: TravelName) : LogsMessage
-
 
     @Serializable
     @SerialName("lobby/join")
